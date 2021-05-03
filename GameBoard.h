1217c4adec140b50e0b6ab29df85a59c1fbae45b
ET <taub.elis@gmail.com>
2021-05-03 15:14:21 -0500

    Minor change to file name and code spacing

--- GameBoard.h
+++ GameBoard.h
@@ -1,4 +1,4 @@
-class Board{
+class GameBoard{
     private:
     int gameBoard[];
     int numPockets;
@@ -6,19 +6,26 @@ class Board{
 
     public:
     //numPockets: number of pockets on each side, numPieces: number of pieces in each pocket
-    Board(int numPockets, int numPieces);
+    GameBoard(int numPockets, int numPieces);
+
     //returns the game board array
     int[] getGameBoard();
+
     //returns the number of pockets on each side
     int getNumPockets();
+
     //returns true if the player has the current turn, false if the computer goes
     bool getCurrentTurn();
+
     //returns true if the game is over
     bool gameOver();
+
     //returns the current score
     int getCurrentScore();
+
     //checks if the spot at that index is playable
     bool spotPlayable(int pocket);
+
     //makes a move starting in the index provided
     void makeMove(int pocket);
 };
